eb8dcfa94e7ef0b26d4ca5aea790580108162283
Janjanny <janjanbeboso0912@gmail.com>
2022-09-28 00:08:30 +0800

    home section not responsive

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,11 +2,6 @@ import "./import-jquery.js";
 
 
 export default function navBar() {
-    // let hamburger = document.querySelector(".hamburger");
-
-    // hamburger.addEventListener('click', () => {
-    //     console.log(1);
-    // });
 
 
     $(".hamburger").click(function() {
