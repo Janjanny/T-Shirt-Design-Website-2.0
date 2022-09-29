f877cf46c8cc6419219d45072ebbf40dbd3fe2de
Janjanny <janjanbeboso0912@gmail.com>
2022-09-29 10:35:40 +0800

    shop responsive ok

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -11382,7 +11382,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56885" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59684" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
