0a7597b7cb15f3996c0ac9486325d5e85375df3b
Janjanny <janjanbeboso0912@gmail.com>
2022-09-29 21:36:01 +0800

    test deploy

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -11382,7 +11382,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58617" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50969" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
